fbdd842d629c72e873fdd0629ffe53c060ee69ef
Barry Moore <moore0557@gmail.com>
2019-12-13 15:43:27 -0500

    Add an hspec test

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,1 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Lib where
-
-import Database
-import Database.SQLite.Simple (open)
-
-example :: IO ()
-example = do
-  conn <- open "test.db"
-  migrateDb conn
-  createProposal conn (Account "bmooreii") (ServiceUnits 10000)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import           Test.Hspec
+import           Database
+import           Database.SQLite.Simple         ( open )
+import           System.Directory               ( removeFile
+                                                , doesFileExist
+                                                )
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Identity
+import           Control.Monad.Trans.Class      ( lift )
+
+newtype FileExists =
+  FileExists
+    { doesExist :: Bool
+    } deriving (Eq, Show)
+
+class MonadIO m => MonadFileExists m where
+  mFileExists :: String -> m FileExists
+
+instance MonadFileExists IO where
+  mFileExists fileName = FileExists <$> doesFileExist fileName
+
+instance MonadFileExists (IdentityT IO) where
+  mFileExists fileName = lift $ FileExists <$> doesFileExist fileName
+
+-- If the test database exists, remove it
+-- try to migrate it
+-- test database should exist
+tryMigrateDb :: MonadFileExists m => m Bool
+tryMigrateDb = do
+  let fileName = "test.db"
+  FileExists remove <- mFileExists fileName
+  if remove then liftIO $ removeFile fileName else pure ()
+  conn <- liftIO $ open "test.db"
+  liftIO $ migrateDb conn
+  FileExists exists <- mFileExists fileName
+  return exists
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ describe "Database Operations" $ do
+  it "should create test.db file" $
+    runIdentityT tryMigrateDb `shouldReturn` True
+  it "test.db should have the correct schema" pending
+  it "should enter an example"                pending
