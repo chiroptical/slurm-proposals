61e13acf9e1bb6ea15a9ef42311d8781d9e7957e
Barry Moore <moore0557@gmail.com>
2020-01-10 16:12:52 -0500

    Remove stuff from Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import           Control.Monad.IO.Class
-import           Database.SQLite.Simple (open)
-
 main :: IO ()
-main = do
+main =
   putStrLn "place holder"
-  -- conn <- liftIO $ open "test.db"
-  -- liftIO $ migrateDb conn
-  -- liftIO $
-  --   insertNewProposal
-  --     conn
-  --     (Account "bmooreii")
-  --     (AccountOwner "bmooreii")
-  --     (ServiceUnits 10000)
-  -- result <- liftIO $ maybeGetProposal conn (Account "bmooreii")
-  -- print result
