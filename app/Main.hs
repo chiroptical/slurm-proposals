4808c6f5c8607f8502a8fc4f4b11023a02433667
Barry Moore <moore0557@gmail.com>
2020-01-16 22:25:01 -0500

    Add subcommands options parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,71 @@
 module Main where
 
-import           Data.Semigroup      ((<>))
+import           Data.Semigroup                  ((<>))
+import           Data.String                     (IsString (..))
+import           Data.Text                       (Text)
 import           Options.Applicative
+import           Options.Applicative.Help.Pretty (text)
 
-newtype Account =
-  Account String
+data Subcommand
+  = Insert
+  | Modify
+  | Add
+  | Get
+  | Reset
+  | Release
+  | Invalid
   deriving (Show)
 
-accountP :: Parser Account
-accountP = Account <$> accountNameOpt
+instance IsString Subcommand where
+  fromString "insert"  = Insert
+  fromString "modify"  = Modify
+  fromString "add"     = Add
+  fromString "get"     = Get
+  fromString "reset"   = Reset
+  fromString "release" = Release
+  fromString _         = Invalid
 
-accountNameOpt =
-  strOption (help "An account" <> long "account" <> metavar "ACCOUNT")
+subcommandParser :: Parser Subcommand
+subcommandParser =
+  argument str $
+  metavar "COMMAND" <>
+  helpDoc
+    (Just
+       (text
+          "\n\
+          \    Valid commands: \n\
+          \      insert:  Insert a new proposal\n\
+          \      modify:  Modify service unit limit\n\
+          \      add:     Add additional service units\n\
+          \      get:     Get the current limit\n\
+          \      reset:   Reset usage in Slurm\n\
+          \      release: Release hold in Slurm"))
 
-opts :: ParserInfo Account
-opts = info accountP (progDesc "something here..." <> header "what is this?")
+data Options =
+  Options
+    { optionsAccount      :: Text
+    , optionsServiceUnits :: Int
+    , optionsSubcommand   :: Subcommand
+    }
+  deriving (Show)
+
+account :: Parser Text
+account =
+  strOption $
+  long "account" <> short 'a' <> metavar "STRING" <>
+  help "The account name for the proposal"
+
+optionsParser :: Parser Options
+optionsParser = Options <$> account <*> serviceUnits <*> subcommandParser
+  where
+    serviceUnits =
+      option auto $
+      long "serviceUnits" <> short 's' <> metavar "NUMBER" <> value 10000 <>
+      showDefault <>
+      help "The number of service units to insert"
+
+opts :: ParserInfo Options
+opts = info (optionsParser <**> helper) (fullDesc <> header "" <> progDesc "")
 
 main :: IO ()
 main = do
